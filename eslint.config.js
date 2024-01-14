67a801f627c53ca092449172831e980b045fd8d7
Jason Garber <jason@sixtwothree.org>
2024-01-14 15:30:05 -0500

    Add test/coverage packages and configuration

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,9 @@ const ava = require('@jgarber/eslint-config/ava');
 const config = require('@jgarber/eslint-config');
 
 module.exports = [
+  {
+    ignores: ['coverage']
+  },
   ...config,
   ...ava
 ];
