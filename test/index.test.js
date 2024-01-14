e2d334b69fcc8f54ca9826fa019e16d3b20f0079
Jason Garber <jason@sixtwothree.org>
2024-01-14 15:30:14 -0500

    Add test suite

---
+++ test/index.test.js
@@ -0,0 +1,59 @@
+const sinon = require('sinon');
+const test = require('ava');
+
+const Eleventy = require('@11ty/eleventy');
+
+test.beforeEach(t => {
+  console.log = sinon.spy();
+});
+
+test.afterEach(t => {
+  sinon.reset();
+});
+
+test('default plugin configuration', async t => {
+  const eleventy = new Eleventy('test/fixtures/default', null, { configPath: 'index.js' });
+  const results = await eleventy.toJSON();
+
+  t.is(results[0].content, 'html {\n  font-family: serif;\n}\n\na {\n  color: #c00;\n}\n');
+
+  t.true(console.log.calledOnce);
+});
+
+test('custom templateFormats option', async t => {
+  const eleventy = new Eleventy('test/fixtures/custom-template-formats', null, {
+    configPath: 'test/fixtures/custom-template-formats/eleventy.config.js'
+  });
+
+  const results = await eleventy.toJSON();
+
+  t.is(results[0].content, 'html {\n  font-family: sans-serif;\n}\n\na {\n  color: rebeccapurple;\n}\n');
+
+  t.true(console.log.calledOnce);
+});
+
+test('Sass-style partials', async t => {
+  const eleventy = new Eleventy('test/fixtures/partials', null, { configPath: 'index.js' });
+  const results = await eleventy.toJSON();
+
+  const expected = [
+    {
+      content: "/* Doesn't matter what's in this file, it shouldn't be written to output. */\n",
+      outputPath: false,
+      url: false
+    },
+    {
+      content: 'html {\n  padding: 1rem;\n}\n\np {\n  font-family: cursive;\n}\n',
+      outputPath: '_site/styles.css',
+      url: '/styles.css'
+    }
+  ];
+
+  t.like(results.sort((a, b) => {
+    if (a.inputPath < b.inputPath) return -1;
+    if (a.inputPath > b.inputPath) return 1;
+    return 0;
+  }), expected);
+
+  t.true(console.log.calledOnce);
+});
