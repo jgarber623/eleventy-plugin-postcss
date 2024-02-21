6831c7475451bc2ef14ae0215ea341a4f885417c
Jason Garber <jason@sixtwothree.org>
2024-02-20 21:31:38 -0500

    Migrate test suite from AVA to Node.js libraries

--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,7 @@
+const assert = require("node:assert");
+const test = require("node:test");
+
 const sinon = require("sinon");
-const test = require("ava");
 
 const Eleventy = require("@11ty/eleventy");
 
@@ -11,49 +13,53 @@ test.afterEach(() => {
   sinon.reset();
 });
 
-test("default plugin configuration", async (t) => {
+test("default plugin configuration", async () => {
   const eleventy = new Eleventy("test/fixtures/default", null, { configPath: "index.js" });
   const results = await eleventy.toJSON();
 
-  t.is(results[0].content, `html {\n  font-family: serif;\n}\n\na {\n  color: #c00;\n}\n`);
+  assert.equal(results[0].content, `html {\n  font-family: serif;\n}\n\na {\n  color: #c00;\n}\n`);
 
-  t.true(console.log.calledOnce);
+  assert.ok(console.log.calledOnce);
 });
 
-test("custom templateFormats option", async (t) => {
+test("custom templateFormats option", async () => {
   const eleventy = new Eleventy("test/fixtures/custom-template-formats", null, {
     configPath: "test/fixtures/custom-template-formats/eleventy.config.js",
   });
 
   const results = await eleventy.toJSON();
 
-  t.is(results[0].content, `html {\n  font-family: sans-serif;\n}\n\na {\n  color: rebeccapurple;\n}\n`);
+  assert.equal(results[0].content, `html {\n  font-family: sans-serif;\n}\n\na {\n  color: rebeccapurple;\n}\n`);
 
-  t.true(console.log.calledOnce);
+  assert.ok(console.log.calledOnce);
 });
 
-test("Sass-style partials", async (t) => {
+test("Sass-style partials", async () => {
   const eleventy = new Eleventy("test/fixtures/partials", null, { configPath: "index.js" });
   const results = await eleventy.toJSON();
+
+  const actual = results.sort((a, b) => {
+    if (a.inputPath < b.inputPath) return -1;
+    if (a.inputPath > b.inputPath) return 1;
+    return 0;
+  });
 
   const expected = [
     {
       content: "/* Doesn't matter what's in this file, it shouldn't be written to output. */\n",
+      inputPath: "./test/fixtures/partials/_partial.css",
       outputPath: false,
       url: false,
     },
     {
       content: `html {\n  padding: 1rem;\n}\n\np {\n  font-family: cursive;\n}\n`,
+      inputPath: "./test/fixtures/partials/styles.css",
       outputPath: "_site/styles.css",
       url: "/styles.css",
     },
   ];
 
-  t.like(results.sort((a, b) => {
-    if (a.inputPath < b.inputPath) return -1;
-    if (a.inputPath > b.inputPath) return 1;
-    return 0;
-  }), expected);
+  assert.deepStrictEqual(actual, expected);
 
-  t.true(console.log.calledOnce);
+  assert.ok(console.log.calledOnce);
 });
