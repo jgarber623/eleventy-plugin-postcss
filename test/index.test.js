8ea0ff93dfd38e87860d1c814ae3434433b19dee
Jason Garber <jason@sixtwothree.org>
2024-02-20 21:42:21 -0500

    Use built-in Node.js mocking instead of Sinon

--- test/index.test.js
+++ test/index.test.js
@@ -1,25 +1,24 @@
 const assert = require("node:assert");
-const test = require("node:test");
-
-const sinon = require("sinon");
+const { mock, test } = require("node:test");
 
 const Eleventy = require("@11ty/eleventy");
 
 test.beforeEach(() => {
-  console.log = sinon.spy();
+  console.log = mock.fn();
 });
 
 test.afterEach(() => {
-  sinon.reset();
+  mock.reset();
 });
 
 test("default plugin configuration", async () => {
   const eleventy = new Eleventy("test/fixtures/default", null, { configPath: "index.js" });
   const results = await eleventy.toJSON();
 
-  assert.equal(results[0].content, `html {\n  font-family: serif;\n}\n\na {\n  color: #c00;\n}\n`);
+  const expected = `html {\n  font-family: serif;\n}\n\na {\n  color: #c00;\n}\n`;
 
-  assert.ok(console.log.calledOnce);
+  assert.strictEqual(results[0].content, expected);
+  assert.strictEqual(console.log.mock.callCount(), 1);
 });
 
 test("custom templateFormats option", async () => {
@@ -29,9 +28,10 @@ test("custom templateFormats option", async () => {
 
   const results = await eleventy.toJSON();
 
-  assert.equal(results[0].content, `html {\n  font-family: sans-serif;\n}\n\na {\n  color: rebeccapurple;\n}\n`);
+  const expected = `html {\n  font-family: sans-serif;\n}\n\na {\n  color: rebeccapurple;\n}\n`;
 
-  assert.ok(console.log.calledOnce);
+  assert.strictEqual(results[0].content, expected);
+  assert.strictEqual(console.log.mock.callCount(), 1);
 });
 
 test("Sass-style partials", async () => {
@@ -60,6 +60,5 @@ test("Sass-style partials", async () => {
   ];
 
   assert.deepStrictEqual(actual, expected);
-
-  assert.ok(console.log.calledOnce);
+  assert.strictEqual(console.log.mock.callCount(), 1);
 });
